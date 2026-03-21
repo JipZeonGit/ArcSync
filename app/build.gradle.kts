8f3de99fbd471102044110cffb711c420b27dcfd
JipZeonGit <jipzeongit@qq.com>
2026-03-21 23:27:19 +0800

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.jipzeongit.arcsync"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     val releaseStoreFile = System.getenv("ARC_RELEASE_STORE_FILE")
@@ -94,3 +94,4 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
+
